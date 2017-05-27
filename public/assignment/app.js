61d03b955e73e876d4b9d4d8d563130e2018aeb6
evrii <ericvanrollinsii@gmail.com>
2017-05-27 15:07:30 -0400

    Updated to Lesson 3.10
    Updated assignment to reflect chnages made through Lesson 3.10.

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,17 +1,4 @@
 (function () {
     angular
-        .module('WAM', ['ngRoute'])
-        .config(configuration);
-
-    function configuration($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'home.html'
-            })
-            .when('/login', {
-                templateUrl: 'user/login.view.client.html',
-                controller: 'loginController',
-                controllerAs: 'model'
-            })
-    }
+        .module('WAM', ['ngRoute']);
 })();
---
+++ public/assignment/views/user/controllers/register.controller.view.js
@@ -0,0 +1,44 @@
+(function () {
+    angular
+        .module('WAM')
+        .controller('registerController', registerController);
+    
+    function registerController($location, userService) {
+        var model = this;
+
+        model.register = register;
+
+        function register (username, password, password2) {
+            if(username === null ||
+                username === '' ||
+                typeof username === 'undefined'){
+                model.error = "Please enter a valid username"
+                return;
+            }
+            var found = userService.findUserByUsername(username);
+
+            if(password === null ||
+                typeof password === 'undefined'){
+                model.error = "Please enter a value for password"
+                return;
+            }
+
+            if(password !== password2) {
+                model.error = "Sorry, your passwords do not match."
+                return;
+            }
+
+            if(found !== null){
+                model.error = "Sorry, it looks like the username '"+ username +"' is already taken."
+            } else {
+                var newUser = {
+                    username: username,
+                    password: password
+                };
+                newUser = userService.createUser(newUser);
+                $location.url('/user/' + newUser._id)
+            }
+        }
+
+    }
+})();
