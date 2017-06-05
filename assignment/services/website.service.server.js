0b3ab087975f51fbcf77159674451b6fcab4dfb2
evrii <ericvanrollinsii@gmail.com>
2017-06-04 23:02:06 -0400

    Added Server Code for Website Service
    Added server code for website service

--- assignment/services/website.service.server.js
+++ assignment/services/website.service.server.js
@@ -11,7 +11,19 @@ var websites = [
     { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }
 ];
 
-app.get("/api/assignment/user/:userId/website", findAllWebsitesForUser)
+app.post('/api/assignment/user/:userId/website', createWebsite);
+app.get('/api/assignment/user/:userId/website', findAllWebsitesForUser);
+app.get('/api/assignment/website/:websiteId', findWebsiteById);
+app.put('/api/assignment/website/:websiteId', updateWebsite);
+app.delete('/api/assignment/website/:websiteId', deleteWebsite);
+
+
+function createWebsite(req, res) {
+    var website = req.body;
+    website._id = (new Date()).getTime() + "";
+    websites.push(user);
+    res.json(website);
+}
 
 function findAllWebsitesForUser(req, res) {
     var userId = req.params['userId'];
@@ -23,5 +35,42 @@ function findAllWebsitesForUser(req, res) {
     }
 
     res.json(results);
+}
+
+function findWebsiteById(req, res) {
+    var websiteId = req.params['websiteId'];
+    for(var w in websites) {
+        if(websites[w]._id === websiteId){
+            res.send(websites[w]);
+            return;
+        }
+    }
+    res.sendStatus(404);
+}
+
+function updateWebsite(req, res) {
+    var website = req.body;
+
+    for(var w in websites) {
+        if(websites[w]._id === req.params['websiteId']){
+            websites[w] = website;
+            res.sendStatus(200);
+            return;
+        }
+    }
+    res.sendStatus(404);
+}
+
+function deleteWebsite(req, res) {
+    var websiteId = req.params['websiteId'];
+
+    for(var w in websites) {
+        if(websites[w]._id === websiteId){
+            websites.splice(u, 1);
+            res.sendStatus(200);
+            return;
+        }
+    }
+    res.sendStatus(404);
 }
 
