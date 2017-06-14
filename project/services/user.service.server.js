2edcc44baf7d0ec04f6765ebfcf5cac26ee81042
evrii <ericvanrollinsii@gmail.com>
2017-06-14 19:00:57 -0400

    Added Basic Database Support
    Added Basic Database Support

--- project/services/user.service.server.js
+++ project/services/user.service.server.js
@@ -1,22 +1,20 @@
-console.log("WE HAVE LIFT OFF!")
 var app = require('../../express');
 var users = [
     {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",
-        lastName: "Wonder", friends: ["234", "345"], followers: ["234"], userType: "student",
+        lastName: "Wonder", friends: ["234", "345"], followers: ["234"], userType: "STUDENT",
         content: []},
     {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",
-        lastName: "Marley",  friends: ["123", "345"], followers: ["123", "345", "456"], userType: "teacher",
+        lastName: "Marley",  friends: ["123", "345"], followers: ["123", "345", "456"], userType: "TEACHER",
         content: ["659290d45a76d72ea5ed68b0e18cdde2"]},
     {_id: "345", username: "charly",   password: "charly",   firstName: "Charly",
-        lastName: "Garcia",  friends: ["234", "123"], followers: ["123", "234", "456"], userType: "student",
+        lastName: "Garcia",  friends: ["234", "123"], followers: ["123", "234", "456"], userType: "STUDENT",
         content: []},
     {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",
-        lastName: "Annunzi", friends: ["234", "345"], followers: [], userType: "teacher",
+        lastName: "Annunzi", friends: ["234", "345"], followers: [], userType: "TEACHER",
         content: []}
 ];
 
-const USER_TYPES = ["teacher", "student", "admin"]
-// Fix this to be a put
+const USER_TYPES = ["TEACHER", "STUDENT", "ADMIN"];
 app.get('/api/project/user/:userId/course/:courseId', addCourse);
 
 app.put('/api/project/user/:userId/friend/:friendId', addFriend);
